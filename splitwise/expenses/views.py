46540e9b4d1088b5de64de02c2ecbad332be4ae4
Lakshya Garg <lakshyarajgarg@gmail.com>
2024-10-20 13:31:15 +0530

    add restframework and some routes

--- splitwise/expenses/views.py
+++ splitwise/expenses/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from expenses import models
 
 # Create your views here.
+
+
+@api_view(["POST"])
+def register_user(request):
+    pass
+
+
+@api_view(["GET"])
+def get_user(request):
+    pass
+
+
+@api_view(["POST"])
+def add_expense(request):
+    pass
+
+
+@api_view(["POST"])
+def user_login(request):
+    pass
