8b48c39de4c2cc23f9a32136306ec3b487043488
Lakshya Garg <lakshyarajgarg@gmail.com>
2024-10-20 20:20:09 +0530

    improve serializer to prevent password hash leak

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -2,22 +2,29 @@ from .models import ExpenseUser, Expense, ExpensePaidBy, ExpenseOwedBy
 
 from rest_framework import serializers
 
+
 class ExpenseUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    createdAt = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = ExpenseUser
-        fields = [ "password" , 'name', 'email', 'phone', 'createdAt']
+        fields = ["name", "email", "phone", "createdAt", "password"]
+
 
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
-        fields = ['expenseId', 'desc', 'amount', 'createdById', 'createdAt']
+        fields = ["expenseId", "desc", "amount", "createdById", "createdAt"]
+
 
 class ExpensePaidBySerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpensePaidBy
-        fields = ['expenseId', 'userId', 'amount']
+        fields = ["expenseId", "userId", "amount"]
+
 
 class ExpenseOwedBySerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpenseOwedBy
-        fields = ['expenseId', 'userId', 'amount']
+        fields = ["expenseId", "userId", "amount"]
