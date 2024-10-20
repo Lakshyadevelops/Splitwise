2a8710335890e5e60e80f9ac46d883a52dcd4c05
Lakshya Garg <lakshyarajgarg@gmail.com>
2024-10-20 19:38:35 +0530

    all routes

--- splitwise/urls.py
+++ splitwise/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for splitwise project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
 from expenses import views
@@ -25,4 +8,7 @@ urlpatterns = [
     path("api/v1/user/login/", views.user_login),
     path("api/v1/user/<int:user_id>/", views.user_get),
     path("api/v1/expense/", views.expense_add),
+    path("api/v1/balance-sheet/<int:user_id>/", views.balance_sheet_get),
+    path("api/v1/balance-sheet/", views.balance_sheet_get_all),
+    path("api/v1/balance-sheet-pdf/", views.balance_sheet_get_all_pdf),
 ]
